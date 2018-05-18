7ff1f6634bc7418131ffb9cd01b4de7be006808f
denghc <13340199938@163.com>
2018-05-18 17:09:11 +0800

    Initial commit

---
+++ src/com/dhc/fmvp/FMVPAction.java
@@ -0,0 +1,138 @@
+package com.dhc.fmvp;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import java.io.*;
+
+public class FMVPAction extends AnAction {
+    Project project;
+    VirtualFile selectGroup;
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        project = e.getProject();
+        String className = Messages.showInputDialog(project, "请输入类名称", "NewMvpGroup", Messages.getQuestionIcon());
+        selectGroup = DataKeys.VIRTUAL_FILE.getData(e.getDataContext());
+        if (className == null || className.equals("")) {
+            System.out.print("没有输入类名");
+            return;
+        }
+        createClassMvp(className);
+        project.getBaseDir().refresh(false, true);
+    }
+
+
+    /**
+     * 根据模块生成代码
+     * @param className
+     */
+    private void createClassMvp(String className) {
+        boolean isFragment = className.endsWith("Fragment") || className.endsWith("fragment");
+        boolean isActivity =className.endsWith("Activity") || className.endsWith("activity");
+        if (className.endsWith("Fragment") || className.endsWith("fragment") || className.endsWith("Activity") || className.endsWith("activity")) {
+            className = className.substring(0, className.length() - 8);
+        }
+        String contractPath = selectGroup.getPath() + "/presenter/contract";
+        String presenterPath = selectGroup.getPath() + "/presenter";
+        String uiPath = selectGroup.getPath() + "/ui";
+        String modlePath = selectGroup.getPath() + "/modle";
+        className = className.substring(0, 1).toUpperCase() + className.substring(1);
+        String contract = readFile("Contract.txt")
+                .replace("&package&", getPackageName(contractPath))
+                .replace("&Contract&", "I"+className + "Contract");
+        String presenter = readFile("Presenter.txt")
+                .replace("&package&",  getPackageName(presenterPath))
+                .replace("&Module&", className)
+                .replace("&Contract&", className + "Contract")
+                .replace("&ContractPackageName&", getPackageName(contractPath))
+                .replace("&DataServicePackageName&", getPackageName(modlePath))
+                .replace("&Presenter&", className + "Presenter");
+        String dataService = readFile("DataService.txt")
+                .replace("&package&",  getPackageName(modlePath))
+                .replace("&Module&", className)
+                .replace("&ContractPackageName&", getPackageName(contractPath))
+                .replace("&Contract&", className + "Contract");
+
+        if (isFragment) {
+            String fragment = readFile("Fragment.txt")
+                    .replace("&package&",  getPackageName(uiPath))
+                    .replace("&Fragment&", className + "Fragment")
+                    .replace("&ContractPackageName&", getPackageName(contractPath))
+                    .replace("&Contract&", className + "Contract")
+                    .replace("&Presenter&", className + "Presenter");
+            writetoFile(fragment, uiPath, className + "Fragment.java");
+        } else if (isActivity){
+            String activity = readFile("Activity.txt")
+                    .replace("&package&",  getPackageName(uiPath))
+                    .replace("&Activity&", className + "Activity")
+                    .replace("&ContractPackageName&", getPackageName(contractPath))
+                    .replace("&Contract&", className + "Contract")
+                    .replace("&Presenter&", className + "Presenter");
+            writetoFile(activity, uiPath, className + "Activity.java");
+        }
+        writetoFile(contract, contractPath, "I"+className + "Contract.java");
+        writetoFile(presenter, presenterPath, className + "Presenter.java");
+        writetoFile(dataService, modlePath, className + "RemoteDataService.java");
+    }
+
+    private String getPackageName(String path) {
+        return path.substring(path.indexOf("java") + 5, path.length()).replace("/", ".");
+    }
+
+
+    private String readFile(String filename) {
+        InputStream in = null;
+        in = this.getClass().getResourceAsStream("code/" + filename);
+        String content = "";
+        try {
+            content = new String(readStream(in));
+        } catch (Exception e) {
+        }
+        return content;
+    }
+
+    private void writetoFile(String content, String filepath, String filename) {
+        try {
+            File floder = new File(filepath);
+            // if file doesnt exists, then create it
+            if (!floder.exists()) {
+                floder.mkdirs();
+            }
+            File file = new File(filepath + "/" + filename);
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(content);
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private byte[] readStream(InputStream inStream) throws Exception {
+        ByteArrayOutputStream outSteam = new ByteArrayOutputStream();
+        try {
+            byte[] buffer = new byte[1024];
+            int len = -1;
+            while ((len = inStream.read(buffer)) != -1) {
+                outSteam.write(buffer, 0, len);
+            }
+
+        } catch (IOException e) {
+        } finally {
+            outSteam.close();
+            inStream.close();
+        }
+        return outSteam.toByteArray();
+    }
+
+}
